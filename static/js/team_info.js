ee344a7700fda2b732d1c33a452a8a2f1e477919
nibir8 <nibir.mukherjee18@gmail.com>
2018-08-10 21:12:39 -0300

    update

--- static/js/team_info.js
+++ static/js/team_info.js
@@ -16,9 +16,19 @@ $(document).on('change',"#country_list",function (e) {
         success: function (response) {
            var r = JSON.parse(response);
             $('#roster tbody').remove();
+			$('#roster thead').remove();
             var table = $("#roster");
-            var tbody = $("<tbody>")
-
+            var tbody = $("<tbody>");
+			var theader = $("<thead>");
+			var header = $("<tr>");
+			
+			for (var col in Object.keys(r)){
+			var head = 	$('<th>');
+			head.text(Object.keys(r)[col]);
+			header.append(head)
+			}
+			theader.append(header);
+			table.append(theader);
             for (var z = 0; z < Object.keys(r["Caps"]).length-1; z++) {
             row = $('<tr>')
             for (var x in r) {
@@ -37,5 +47,3 @@ $(document).on('change',"#country_list",function (e) {
         }
     });
     });
-
-
